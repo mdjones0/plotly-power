9040d3a788d02f55316fe2127777db7bb70cd66f
MD Jones <jonesm@buffalo.edu>
2022-08-31 16:37:34 -0400

    updated at 2022-08-31T16:37:34-0400

--- plotindpower.py
+++ plotindpower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # plotter for mri cluster pdu power
+#  upgraded pandas series version
 #
 #format of pdupower-YYYYMMDD
 #1538595061,u22,10.90,363,417,310
@@ -27,7 +28,13 @@ import csv
 from datetime import datetime
 import pytz
 
+from scipy import signal
+
 py.plotly.tools.set_credentials_file(username='jonesm', api_key='f91fad0x9z')
+
+def f(i):
+    return pd.read_csv(i, header=None)
+
 #x = [
 #    datetime(year=2013, month=10, day=04),
 #    datetime(year=2013, month=11, day=05),
@@ -61,35 +68,40 @@ rack_time = {new_list: [] for new_list in ("m22","h22","h23","h24","h25", \
 #rack_load = dict()
 #rack_time2 = dict()
 #rack_load2 = dict()
+rack_load_resampled = {new_list: [] for new_list in ("h22", "h23", "h24", "h25")}
+rack_time_resampled = {new_list: [] for new_list in ("h22", "h23", "h24", "h25")}
 
-LOGDIR = "/srv/cosmos/logging/pdupower"
+#LOGDIR = "/srv/cosmos/logging/pdupower"
+LOGDIR = "/projects/ccrstaff/jonesm/d_stats/d_pdus-mirror/pdudata/"
 # this parsing is really slow, try pandas to see if it speeds up?
 # https://stackoverflow.com/questions/2473783/is-there-a-way-to-circumvent-python-list-append-becoming-progressively-slower
 # turning gc off does not seem to help
 #gc.disable()
 #  pre-defining lists really helped, now constant per file
-for infilename in sorted(glob.glob("/srv/cosmos/logging/pdudata/pdupower-202107*"), key=os.path.getmtime) + \
-	sorted(glob.glob("/srv/cosmos/logging/pdudata/pdupower-20210[8-9]*"), key=os.path.getmtime) + \
-	sorted(glob.glob("/srv/cosmos/logging/pdudata/pdupower-20211[0-2]*"), key=os.path.getmtime) + \
-	sorted(glob.glob("/srv/cosmos/logging/pdudata/pdupower-2022*"), key=os.path.getmtime):
-    t0 = time.time()
-    with open(infilename) as infile:
-        allrecords = infile.readlines()[:] # no header
-	# sam's log format is:
-	# time,rack,kW
-        for line in allrecords:
-            line_words = line.split(',')
-            #print 'line_words[0] = ',line_words[0]
-            # epoch time is first, convert
-            dt = datetime.fromtimestamp(float(line_words[0]), tz)
-            date_string = dt.strftime('%Y-%m-%dT%H:%M:%S')
-            rack = line_words[1]
-            load = float(line_words[2])
-            rack_load[rack].append(load)
-            rack_time[rack].append(date_string)
+dayfile_count = 0
+#for infilename in sorted(glob.glob(LOGDIR + "pdupower-202107*"), key=os.path.getmtime) + \
+#	sorted(glob.glob(LOGDIR + "pdupower-20210[8-9]*"), key=os.path.getmtime) + \
+#	sorted(glob.glob(LOGDIR + "pdupower-20211[0-2]*"), key=os.path.getmtime) + \
+#	sorted(glob.glob(LOGDIR + "pdupower-202[2-9]*"), key=os.path.getmtime):
+#    dayfile_count += 1
+#    t0 = time.time()
+#    with open(infilename) as infile:
+#        allrecords = infile.readlines()[:] # no header
+#	# sam's log format is:
+#	# time,rack,kW
+#        for line in allrecords:
+#            line_words = line.split(',')
+#            #print 'line_words[0] = ',line_words[0]
+#            # epoch time is first, convert
+#            dt = datetime.fromtimestamp(float(line_words[0]), tz)
+#            date_string = dt.strftime('%Y-%m-%dT%H:%M:%S')
+#            rack = line_words[1]
+#            load = float(line_words[2])
+#            rack_load[rack].append(load)
+#            rack_time[rack].append(date_string)
 
-    t1=time.time()
-    print("0. %f secs for file %s" % (t1-t0,infilename))
+#    t1=time.time()
+#    print("0. %f secs for file %s" % (t1-t0,infilename))
 
     # pandas version only about 10% faster
     #t2 = time.time()
@@ -105,30 +117,69 @@ for infilename in sorted(glob.glob("/srv/cosmos/logging/pdudata/pdupower-202107*
     #t3 = time.time()
     #print("1. %f secs for file %s" % (t3-t2,infilename))
 
+# big pandas glob of all the data
+t0 = time.time()
+joined_files = os.path.join(LOGDIR,"pdupower-202[1-9]*")
+joined_files_list = glob.glob(joined_files)
+print("Number of globbed files: ",len(joined_files_list))
+df = pd.concat(map(f, joined_files_list))
+t1 = time.time()
+print("Time to glob and read in data: %f secs" % (t1-t0))
+
+#print(df)
+#df.reset_index()
+df.columns = ['Date','Rack','PowerkW']
+df.index = pd.to_datetime(df['Date'],unit='s')
+#df['timestamp'] = pd.to_datetime(df['Date'],unit='s')
+
+df_h22 = df[df["Rack"] == "h22"].resample('10T').mean()
+df_h23 = df[df["Rack"] == "h23"].resample('10T').mean()
+df_h24 = df[df["Rack"] == "h24"].resample('10T').mean()
+df_h25 = df[df["Rack"] == "h25"].resample('10T').mean()
+
+print(df_h22)
+
+print("Number of data points for H22: ",len(df_h22["PowerkW"]))
 #gc.enable()
 #print(rack_time["h22"],rack_load["h22"])
+
+#print("Number of files read (one per day): ",dayfile_count)
+#print("Number of hours,min,sec in period: ",dayfile_count*24,dayfile_count*24*60,dayfile_count*24*60*60)
+#print("Number of data points in h22: ",len(rack_load["h22"]))
+
+#
+# resample to cut down on size (or should we just toss some data points?):
+#rack_load_resampled["h22"] = signal.resample(rack_load["h22"],int(len(rack_load["h22"])/2))
+#rack_time_resampled["h22"] = signal.resample(rack_time["h22"],int(len(rack_time["h22"])/2))
+#rack_load_resampled["h23"] = signal.resample(rack_load["h23"],int(len(rack_load["h23"])/2))
+#rack_time_resampled["h23"] = signal.resample(rack_time["h23"],int(len(rack_time["h23"])/2))
+#rack_load_resampled["h24"] = signal.resample(rack_load["h24"],int(len(rack_load["h24"])/2))
+#rack_time_resampled["h24"] = signal.resample(rack_time["h24"],int(len(rack_load["h24"])/2))
+#rack_load_resampled["h25"] = signal.resample(rack_load["h25"],int(len(rack_load["h25"])/2))
+#rack_load_resampled["h25"] = signal.resample(rack_time["h25"],int(len(rack_time["h25"])/2))
+
 trace1 = go.Scattergl(
-    x=rack_time["h22"],
-    y=rack_load["h22"],
-    name='Rack h22',
+    x=df_h22.index,
+    y=df_h22["PowerkW"],
+    name='Rack H22',
     mode='lines+markers'
 )
 trace2 = go.Scattergl(
-    x=rack_time["h23"],
-    y=rack_load["h23"],
-    name='Rack h23',
+    x=df_h23.index,
+    y=df_h23["PowerkW"],
+    name='Rack H23',
     mode='lines+markers'
 )
 trace3 = go.Scattergl(
-    x=rack_time["h24"],
-    y=rack_load["h24"],
-    name='Rack h24',
+    x=df_h24.index,
+    y=df_h24["PowerkW"],
+    name='Rack H24',
     mode='lines+markers'
 )
 trace4 = go.Scattergl(
-    x=rack_time["h25"],
-    y=rack_load["h25"],
-    name='Rack h25',
+    x=df_h25.index,
+    y=df_h25["PowerkW"],
+    name='Rack H25',
     mode='lines+markers'
 )
 	
